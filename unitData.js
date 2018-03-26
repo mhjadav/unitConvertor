d4bcce2ac7be1df80c8643eea6a7c15253230500
Mahipatsinh Jadav <mhjadav@ymail.com>
2018-03-26 22:00:39 +0530

    Add files via upload

---
+++ unitData.js
@@ -0,0 +1,774 @@
+var unitData = {  
+    "Acceleration":{  
+       "Meter/sq.sec (m/sec^2)":{  
+          'factor':1
+       },
+       "Foot/sq.sec (ft/sec^2)":{  
+          'factor':0.3048
+       },
+       "G (g)":{  
+          'factor':9.80665
+       },
+       "Galileo (gal)":{  
+          'factor':0.01
+       },
+       "Inch/sq.sec (in/sec^2)":{  
+          'factor':0.0254
+       }
+    },
+    "Area":{  
+       "Square meter (m^2)":{  
+          'factor':1
+       },
+       "Acre (acre)":{  
+          'factor':4046.856
+       },
+       "Are":{  
+          'factor':100
+       },
+       "Barn (barn)":{  
+          'factor':1e-28
+       },
+       "Hectare":{  
+          'factor':10000
+       },
+       "Rood":{  
+          'factor':1011.71413184285
+       },
+       "Square centimeter":{  
+          'factor':0.0001
+       },
+       "Square kilometer":{  
+          'factor':1000000
+       },
+       "Circular mil":{  
+          'factor':5.067075e-10
+       },
+       "Square foot (ft^2)":{  
+          'factor':0.09290304
+       },
+       "Square inch (in^2)":{  
+          'factor':0.00064516
+       },
+       "Square mile (mi^2)":{  
+          'factor':2589988
+       },
+       "Square yard (yd^2)":{  
+          'factor':0.8361274
+       }
+    },
+    "Torque":{  
+       "Newton-meter (N m)":{  
+          'factor':1
+       },
+       "Dyne-centimeter(dy cm)":{  
+          'factor':1e-7
+       },
+       "Kgrf-meter (kgf m)":{  
+          'factor':9.80665
+       },
+       "lbf-inch (lbf in)":{  
+          'factor':0.1129848
+       },
+       "lbf-foot (lbf ft)":{  
+          'factor':1.355818
+       }
+    },
+    "Electricity":{  
+       "Coulomb (Cb)":{  
+          'factor':1
+       },
+       "Abcoulomb":{  
+          'factor':10
+       },
+       "Ampere hour (A hr)":{  
+          'factor':3600
+       },
+       "Faraday (F)":{  
+          'factor':96521.8999999997
+       },
+       "Statcoulomb":{  
+          'factor':3.33564e-10
+       },
+       "Millifaraday (mF)":{  
+          'factor':96.5219
+       },
+       "Microfaraday (mu-F)":{  
+          'factor':0.0965219
+       },
+       "Picofaraday (pF)":{  
+          'factor':0.0000965219
+       }
+    },
+    "Energy":{  
+       "Joule (J)":{  
+          'factor':1
+       },
+       "BTU (mean)":{  
+          'factor':1055.87
+       },
+       "BTU (thermochemical)":{  
+          'factor':1054.35
+       },
+       "Calorie (SI) (cal)":{  
+          'factor':4.1868
+       },
+       "Calorie (mean)(cal)":{  
+          'factor':4.19002
+       },
+       "Calorie (thermo)":{  
+          'factor':4.184
+       },
+       "Electron volt (eV)":{  
+          'factor':1.6021e-19
+       },
+       "Erg (erg)":{  
+          'factor':1e-7
+       },
+       "Foot-pound force":{  
+          'factor':1.355818
+       },
+       "Foot-poundal":{  
+          'factor':0.04214011
+       },
+       "Horsepower-hour":{  
+          'factor':2684077.3
+       },
+       "Kilocalorie (SI)(kcal)":{  
+          'factor':4186.8
+       },
+       "Kilocalorie (mean)(kcal)":{  
+          'factor':4190.02
+       },
+       "Kilowatt-hour (kW hr)":{  
+          'factor':3600000
+       },
+       "Ton of TNT":{  
+          'factor':4200000000
+       },
+       "Volt-coulomb (V Cb)":{  
+          'factor':1
+       },
+       "Watt-hour (W hr)":{  
+          'factor':3600
+       },
+       "Watt-second (W sec)":{  
+          'factor':1
+       }
+    },
+    "Force":{  
+       "Newton (N)":{  
+          'factor':1
+       },
+       "Dyne (dy)":{  
+          'factor':0.00001
+       },
+       "Kilogram force (kgf)":{  
+          'factor':9.80665
+       },
+       "Kilopond force (kpf)":{  
+          'factor':9.80665
+       },
+       "Kip (k)":{  
+          'factor':4448.222
+       },
+       "Ounce force (ozf)":{  
+          'factor':0.2780139
+       },
+       "Pound force (lbf)":{  
+          'factor':0.4535924
+       },
+       "Poundal":{  
+          'factor':0.138255
+       }
+    },
+    "Force / Length":{  
+       "Newton/meter (N/m)":{  
+          'factor':1
+       },
+       "Pound force/inch (lbf/in)":{  
+          'factor':175.1268
+       },
+       "Pound force/foot (lbf/ft)":{  
+          'factor':14.5939
+       }
+    },
+    "Length":{  
+       "Meter (m)":{  
+          'factor':1
+       },
+       "Angstrom (A')":{  
+          'factor':1e-10
+       },
+       "Astronomical unit (AU)":{  
+          'factor':149598000000
+       },
+       "Caliber (cal)":{  
+          'factor':0.000254
+       },
+       "Centimeter (cm)":{  
+          'factor':0.01
+       },
+       "Kilometer (km)":{  
+          'factor':1000
+       },
+       "Ell":{  
+          'factor':1.143
+       },
+       "Em":{  
+          'factor':0.0042323
+       },
+       "Fathom":{  
+          'factor':1.8288
+       },
+       "Furlong":{  
+          'factor':201.168
+       },
+       "Fermi (fm)":{  
+          'factor':1e-15
+       },
+       "Foot (ft)":{  
+          'factor':0.3048
+       },
+       "Inch (in)":{  
+          'factor':0.0254
+       },
+       "League (int'l)":{  
+          'factor':5556
+       },
+       "League (UK)":{  
+          'factor':5556
+       },
+       "Light year (LY)":{  
+          'factor':9460550000000000
+       },
+       "Micrometer (mu-m)":{  
+          'factor':0.000001
+       },
+       "Mil":{  
+          'factor':0.0000254
+       },
+       "Millimeter (mm)":{  
+          'factor':0.001
+       },
+       "Nanometer (nm)":{  
+          'factor':1e-9
+       },
+       "Mile (int'l nautical)":{  
+          'factor':1852
+       },
+       "Mile (UK nautical)":{  
+          'factor':1853.184
+       },
+       "Mile (US nautical)":{  
+          'factor':1852
+       },
+       "Mile (US statute)":{  
+          'factor':1609.344
+       },
+       "Parsec":{  
+          'factor':30837400000000000
+       },
+       "Pica (printer)":{  
+          'factor':0.004217518
+       },
+       "Picometer (pm)":{  
+          'factor':1e-12
+       },
+       "Point (pt)":{  
+          'factor':0.0003514598
+       },
+       "Rod":{  
+          'factor':5.0292
+       },
+       "Yard (yd)":{  
+          'factor':0.9144
+       }
+    },
+    "Light":{  
+       "Lumen/sq.meter (Lu/m^2)":{  
+          'factor':1
+       },
+       "Lumen/sq.centimeter":{  
+          'factor':10000
+       },
+       "Lumen/sq.foot":{  
+          'factor':10.76391
+       },
+       "Foot-candle (ft-cdl)":{  
+          'factor':10.76391
+       },
+       "Foot-lambert":{  
+          'factor':10.76391
+       },
+       "Candela/sq.meter":{  
+          'factor':3.14159250538575
+       },
+       "Candela/sq.centimeter":{  
+          'factor':31415.9250538576
+       },
+       "Lux (lux)":{  
+          'factor':1
+       },
+       "Phot":{  
+          'factor':10000
+       }
+    },
+    "Mass":{  
+       "Kilogram (kgr)":{  
+          'factor':1
+       },
+       "Gram (gr)":{  
+          'factor':0.001
+       },
+       "Milligram (mgr)":{  
+          'factor':0.000001
+       },
+       "Microgram (mu-gr)":{  
+          'factor':1e-9
+       },
+       "Carat (metric)(ct)":{  
+          'factor':0.0002
+       },
+       "Hundredweight (long)":{  
+          'factor':50.80235
+       },
+       "Hundredweight (short)":{  
+          'factor':45.35924
+       },
+       "Pound mass (lbm)":{  
+          'factor':0.4535924
+       },
+       "Pound mass (troy)":{  
+          'factor':0.3732417
+       },
+       "Ounce mass (ozm)":{  
+          'factor':0.02834952
+       },
+       "Ounce mass (troy)":{  
+          'factor':0.03110348
+       },
+       "Slug":{  
+          'factor':14.5939
+       },
+       "Ton (assay)":{  
+          'factor':0.02916667
+       },
+       "Ton (long)":{  
+          'factor':1016.047
+       },
+       "Ton (short)":{  
+          'factor':907.1847
+       },
+       "Ton (metric)":{  
+          'factor':1000
+       },
+       "Tonne":{  
+          'factor':1000
+       }
+    },
+    "Mass Flow":{  
+       "Kilogram/second (kgr/sec)":{  
+          'factor':1
+       },
+       "Pound mass/sec (lbm/sec)":{  
+          'factor':0.4535924
+       },
+       "Pound mass/min (lbm/min)":{  
+          'factor':0.007559873
+       }
+    },
+    "Density & Mass capacity":{  
+       "Kilogram/cub.meter":{  
+          'factor':1
+       },
+       "Grain/galon":{  
+          'factor':0.01711806
+       },
+       "Grams/cm^3 (gr/cc)":{  
+          'factor':1000
+       },
+       "Pound mass/cubic foot":{  
+          'factor':16.01846
+       },
+       "Pound mass/cubic-inch":{  
+          'factor':27679.91
+       },
+       "Ounces/gallon (UK,liq)":{  
+          'factor':6.236027
+       },
+       "Ounces/gallon (US,liq)":{  
+          'factor':7.489152
+       },
+       "Ounces (mass)/inch":{  
+          'factor':1729.994
+       },
+       "Pound mass/gal (UK,liq)":{  
+          'factor':99.77644
+       },
+       "Pound mass/gal (US,liq)":{  
+          'factor':119.8264
+       },
+       "Slug/cubic foot":{  
+          'factor':515.379
+       },
+       "Tons (long,mass)/cub.yard":{  
+          'factor':1328.939
+       }
+    },
+    "Power":{  
+       "Watt (W)":{  
+          'factor':1
+       },
+       "Kilowatt (kW)":{  
+          'factor':1000
+       },
+       "Megawatt (MW)":{  
+          'factor':1000000
+       },
+       "Milliwatt (mW)":{  
+          'factor':0.001
+       },
+       "BTU (SI)/hour":{  
+          'factor':0.2930667
+       },
+       "BTU (thermo)/second":{  
+          'factor':1054.35
+       },
+       "BTU (thermo)/minute":{  
+          'factor':17.5725
+       },
+       "BTU (thermo)/hour":{  
+          'factor':0.2928751
+       },
+       "Calorie (thermo)/second":{  
+          'factor':4.184
+       },
+       "Calorie (thermo)/minute":{  
+          'factor':0.06973333
+       },
+       "Erg/second":{  
+          'factor':1e-7
+       },
+       "Foot-pound force/hour":{  
+          'factor':0.0003766161
+       },
+       "Foot-pound force/minute":{  
+          'factor':0.02259697
+       },
+       "Foot-pound force/second":{  
+          'factor':1.355818
+       },
+       "Horsepower(550 ft lbf/s)":{  
+          'factor':745.7
+       },
+       "Horsepower (electric)":{  
+          'factor':746
+       },
+       "Horsepower (boiler)":{  
+          'factor':9809.5
+       },
+       "Horsepower (metric)":{  
+          'factor':735.499
+       },
+       "Horsepower (UK)":{  
+          'factor':745.7
+       },
+       "Kilocalorie (thermo)/min":{  
+          'factor':69.7333
+       },
+       "Kilocalorie (thermo)/sec":{  
+          'factor':4184
+       }
+    },
+    "Pressure & Stress":{  
+       "Newton/sq.meter":{  
+          'factor':1
+       },
+       "Atmosphere (normal)":{  
+          'factor':101325
+       },
+       "Atmosphere (techinical)":{  
+          'factor':98066.5
+       },
+       "Bar":{  
+          'factor':100000
+       },
+       "Centimeter mercury(cmHg)":{  
+          'factor':1333.22
+       },
+       "Centimeter water (4'C)":{  
+          'factor':98.0638
+       },
+       "Decibar":{  
+          'factor':10000
+       },
+       "Kgr force/sq.centimeter":{  
+          'factor':98066.5
+       },
+       "Kgr force/sq.meter":{  
+          'factor':9.80665
+       },
+       "Kip/square inch":{  
+          'factor':6894757
+       },
+       "Millibar":{  
+          'factor':100
+       },
+       "Millimeter mercury(mmHg)":{  
+          'factor':133.3224
+       },
+       "Pascal (Pa)":{  
+          'factor':1
+       },
+       "Kilopascal (kPa)":{  
+          'factor':1000
+       },
+       "Megapascal (Mpa)":{  
+          'factor':1000000
+       },
+       "Poundal/sq.foot":{  
+          'factor':47.88026
+       },
+       "Pound-force/sq.foot":{  
+          'factor':47.88026
+       },
+       "Pound-force/sq.inch (psi)":{  
+          'factor':6894.757
+       },
+       "Torr (mmHg,0'C)":{  
+          'factor':133.322
+       }
+    },
+    "Temperature":{  
+       "Degrees Celsius ('C)":{  
+           'name':"Celsius",
+       },
+       "Degrees Fahrenheit ('F)":{  
+          'name':"Fahrenheit"
+       },
+       "Degrees Kelvin ('K)":{  
+           'name': 'Kelvin'
+       },
+       "Degrees Rankine ('R)":{  
+           'name': 'Rankine'
+       }
+    },
+    "Time":{  
+       "Second (sec)":{  
+          'factor':1
+       },
+       "Day (mean solar)":{  
+          'factor':86400
+       },
+       "Day (sidereal)":{  
+          'factor':86164.09
+       },
+       "Hour (mean solar)":{  
+          'factor':3600
+       },
+       "Hour (sidereal)":{  
+          'factor':3590.17
+       },
+       "Minute (mean solar)":{  
+          'factor':60
+       },
+       "Minute (sidereal)":{  
+          'factor':60
+       },
+       "Month (mean calendar)":{  
+          'factor':2628000
+       },
+       "Second (sidereal)":{  
+          'factor':0.9972696
+       },
+       "Year (calendar)":{  
+          'factor':31536000
+       },
+       "Year (tropical)":{  
+          'factor':31556930
+       },
+       "Year (sidereal)":{  
+          'factor':31558150
+       }
+    },
+    "Velocity & Speed":{  
+       "Meter/second (m/sec)":{  
+          'factor':1
+       },
+       "Foot/minute (ft/min)":{  
+          'factor':0.00508
+       },
+       "Foot/second (ft/sec)":{  
+          'factor':0.3048
+       },
+       "Kilometer/hour (kph)":{  
+          'factor':0.2777778
+       },
+       "Knot (int'l)":{  
+          'factor':0.5144444
+       },
+       "Mile (US)/hour (mph)":{  
+          'factor':0.44707
+       },
+       "Mile (nautical)/hour":{  
+          'factor':0.514444
+       },
+       "Mile (US)/minute":{  
+          'factor':26.8224
+       },
+       "Mile (US)/second":{  
+          'factor':1609.344
+       },
+       "Speed of light (c)":{  
+          'factor':299792458
+       },
+       "Mach (STP)(a)":{  
+          'factor':340.006875
+       }
+    },
+    "Viscosity":{  
+       "Newton-second/meter":{  
+          'factor':1
+       },
+       "Centipoise":{  
+          'factor':0.001
+       },
+       "Centistoke":{  
+          'factor':0.000001
+       },
+       "Sq.foot/second":{  
+          'factor':0.09290304
+       },
+       "Poise":{  
+          'factor':0.1
+       },
+       "Poundal-second/sq.foot":{  
+          'factor':1.488164
+       },
+       "Pound mass/foot-second":{  
+          'factor':1.488164
+       },
+       "Pound force-second/sq.foot":{  
+          'factor':47.88026
+       },
+       "Rhe":{  
+          'factor':10
+       },
+       "Slug/foot-second":{  
+          'factor':47.88026
+       },
+       "Stoke":{  
+          'factor':0.0001
+       }
+    },
+    "Volume & Capacity":{  
+       "Cubic Meter (m^3)":{  
+          'factor':1
+       },
+       "Cubic centimeter":{  
+          'factor':0.000001
+       },
+       "Cubic millimeter":{  
+          'factor':1e-9
+       },
+       "Acre-foot":{  
+          'factor':1233.482
+       },
+       "Barrel (oil)":{  
+          'factor':0.1589873
+       },
+       "Board foot":{  
+          'factor':0.002359737
+       },
+       "Bushel (US)":{  
+          'factor':0.03523907
+       },
+       "Cup":{  
+          'factor':0.0002365882
+       },
+       "Fluid ounce (US)":{  
+          'factor':0.00002957353
+       },
+       "Cubic foot":{  
+          'factor':0.02831685
+       },
+       "Gallon (UK)":{  
+          'factor':0.004546087
+       },
+       "Gallon (US,dry)":{  
+          'factor':0.004404884
+       },
+       "Gallon (US,liq)":{  
+          'factor':0.003785412
+       },
+       "Gill (UK)":{  
+          'factor':0.0001420652
+       },
+       "Gill (US)":{  
+          'factor':0.0001182941
+       },
+       "Cubic inch (in^3)":{  
+          'factor':0.00001638706
+       },
+       "Liter (new)":{  
+          'factor':0.001
+       },
+       "Liter (old)":{  
+          'factor':0.001000028
+       },
+       "Ounce (UK,fluid)":{  
+          'factor':0.00002841305
+       },
+       "Ounce (US,fluid)":{  
+          'factor':0.00002957353
+       },
+       "Peck (US)":{  
+          'factor':0.008809768
+       },
+       "Pint (US,dry)":{  
+          'factor':0.0005506105
+       },
+       "Pint (US,liq)":{  
+          'factor':0.0004731765
+       },
+       "Quart (US,dry)":{  
+          'factor':0.001101221
+       },
+       "Quart (US,liq)":{  
+          'factor':0.000946353
+       },
+       "Stere":{  
+          'factor':1
+       },
+       "Tablespoon":{  
+          'factor':0.00001478676
+       },
+       "Teaspoon":{  
+          'factor':0.000004928922
+       },
+       "Ton (register)":{  
+          'factor':2.831685
+       },
+       "Cubic yard":{  
+          'factor':0.7645549
+       }
+    },
+    "Volume Flow":{  
+       "Cubic meter/second":{  
+          'factor':1
+       },
+       "Cubic foot/second":{  
+          'factor':0.02831685
+       },
+       "Cubic foot/minute":{  
+          'factor':0.0004719474
+       },
+       "Cubic inches/minute":{  
+          'factor':2.731177e-7
+       },
+       "Gallons (US,liq)/minute)":{  
+          'factor':0.0000630902
+       }
+    }
+ }
---
+++ unitconverter.js
@@ -0,0 +1,152 @@
+/*
+Here is the cases
+
+On category change
+Value in first form is source value and value in second form is target value
+
+On Unit change 
+If first form unit change then value in second form is source value and value in first form is target value
+If second form unit change then valuer in first form is source value and value in second form is target value
+
+On value change (User input some value)
+On value enter in first form input value in second form input got change
+On value enter in second form input value in firt form input got change
+
+*/
+
+(function(){
+    var  keysAllowedArray = [190, 189];
+    var fixedDecimal = 14;
+    var lastValueFirstInput = 1;
+    var lastValueSecondInput = 1;
+    function UpdateUnitMenu(propMenu, unitMenu){
+        FillMenuWithArray(unitMenu, unitData[propMenu.value]);
+        if(document.form_A.unit_input.value){
+            CalculateUnit(document.form_A, document.form_B);
+        }
+    }
+    function onFirstFormValueChange(event){
+        var value = replaceComma(this.value);
+        if ( !isNaN(value) || value == 0){
+            this.value = addThousandSeparator(parseFloat(value));
+        }
+    };
+    function onSecondFormValueChange(event){
+        var value = replaceComma(this.value);
+        if ( !isNaN(value) || value == 0){
+            this.value = addThousandSeparator(parseFloat(value));
+        }
+    };
+    function onFirstFormValueKeyUp(event){
+        if(keysAllowedArray.indexOf(event.keyCode) > -1){
+            return false
+        }
+        if(document.form_A.unit_input.value){
+            CalculateUnit(document.form_A, document.form_B);
+        }
+    }
+    function onFirstFormUnitChange(){
+        if(document.form_A.unit_input.value){
+            CalculateUnit(document.form_B, document.form_A);
+        }
+    }
+    function onSecondFormValueKeyUp(event){
+        if(keysAllowedArray.indexOf(event.keyCode) > -1){
+            return false
+        }
+        if(document.form_B.unit_input.value){
+            CalculateUnit(document.form_B, document.form_A);
+        }
+    }
+    function onSecondFormUnitChange(){
+        if(document.form_B.unit_input.value){
+            CalculateUnit(document.form_A, document.form_B);
+        }
+    }
+    window.onload = function(){
+        document.form_A.unit_menu.onchange = onFirstFormUnitChange;
+        document.form_A.unit_input.onkeyup = onFirstFormValueKeyUp;
+        document.form_A.unit_input.onchange = onFirstFormValueChange;
+        document.form_B.unit_menu.onchange = onSecondFormUnitChange;
+        document.form_B.unit_input.onkeyup = onSecondFormValueKeyUp;
+        document.form_B.unit_input.onchange = onSecondFormValueChange;
+    }
+    function FillMenuWithArray(myMenu, unitsObject){
+        var unitMenutTextArray = Object.keys(unitsObject); 
+        myMenu.length = unitMenutTextArray.length;
+        unitMenutTextArray.forEach(function(value, index){
+            myMenu.options[index].text = value;
+            if(unitsObject[value].factor){
+                myMenu.options[index].value = unitsObject[value].factor;                
+            }
+        });
+    }
+
+    function CalculateUnit(sourceForm, targetForm){
+        var sourceValue = sourceForm.unit_input.value;
+        sourceValue = parseFloat(sourceValue);
+        if ( !isNaN(sourceValue) || sourceValue == 0){
+            ConvertFromTo(sourceForm, targetForm);
+        } else {
+            alert("What you gave me cannot be converted or is zero!");
+        }
+    }
+    
+    function ConvertFromTo(sourceForm, targetForm){
+        if(targetForm.unit_menu.selectedIndex > -1 && sourceForm.unit_menu.selectedIndex > -1){
+            var propValue = document.property_form.the_menu.value;
+            var sourceFactor = sourceForm.unit_menu.value;
+            var sourceText = sourceForm.unit_menu.options[sourceForm.unit_menu.selectedIndex].text;
+            var targetFactor = targetForm.unit_menu.value;
+            var targetText = targetForm.unit_menu.options[targetForm.unit_menu.selectedIndex].text;
+            var result = replaceComma(sourceForm.unit_input.value);
+            
+            if (propValue == "Temperature"){
+                result = parseFloat(result) + unitData[propValue][sourceText].increment;
+            }
+            
+            result = result * sourceFactor;
+            result = result / targetFactor;
+            
+            if (propValue == "Temperature"){
+                if(unitData[propValue][targetText]){
+                    result = parseFloat(result) - unitData[propValue][targetText].increment;
+                }
+            }            
+            targetForm.unit_input.value = addThousandSeparator(result);
+            lastValueFirstInput = document.form_A.unit_input.value;
+            lastValueSecondInput = document.form_B.unit_input.value;
+            console.log(lastValueSecondInput,lastValueFirstInput);
+            
+        }
+    }
+
+    function addThousandSeparator(number) {
+        if(number <= 9999){
+            return parseFloat(number.toFixed(fixedDecimal));
+        }
+        return parseFloat(number.toFixed(fixedDecimal)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    function replaceComma(numberString) {
+        return parseFloat(numberString.replace(/,/g,""));
+    }
+
+    function ClearForm(){
+        document.form_A.unit_input.value = "1";
+        document.form_B.unit_input.value = "1";
+    }
+
+    function onSliderChange() {
+        var output = document.getElementById("lbldecimalslider");
+        output.innerHTML = this.value;
+        fixedDecimal = this.value;
+        document.form_A.unit_input.value = addThousandSeparator(replaceComma(lastValueFirstInput));
+        document.form_B.unit_input.value = addThousandSeparator(replaceComma(lastValueSecondInput));
+    }
+    window.ClearForm = ClearForm;
+    window.FillMenuWithArray = FillMenuWithArray;
+    window.UpdateUnitMenu = UpdateUnitMenu;
+    window.CalculateUnit = CalculateUnit;
+    window.onSliderChange = onSliderChange;
+})();
